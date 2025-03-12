30449c5bdd2af0be64c973d16d364760a4f132ac
ajgogia <ajgogia@deloitte.com>
2025-03-13 03:42:20 +0530

    xyz

--- src/components/User.js
+++ src/components/User.js
@@ -3,10 +3,25 @@ import React from "react";
 class User extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            userInfo: {
+                name: 'Dummy',
+                location: 'Dummy'
+            }
+        }
     }
 
+    async componentDidMount() {
+        const data = await fetch('https://api.github.com/users/ajaygogia')
+        const json = await data.json()
+        this.setState({
+            userInfo: json
+        })
+    }
+
+
     render() {
-        const { name, location } = this.props
+        const { name, location } = this.state.userInfo
         return (
             <div className="user-card">
                 <h1>Name: {name}</h1>
