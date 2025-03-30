d87f2fe6c5082da2d3975a9d9cc55cf50f9ce42c
ajgogia <ajgogia@deloitte.com>
2025-03-30 23:19:18 +0530

    TEST

--- src/__tests__/header.test.js
+++ src/__tests__/header.test.js
@@ -1,5 +1,21 @@
+import { TextEncoder, TextDecoder } from 'util'
+global.TextEncoder = TextEncoder
+global.TextDecoder = { prototype: TextDecoder }
 
-test ('Calculates xyz', ()=>{
-    const result = sum(2,8)
-    expect(result).toBe(10)
+import { render } from "@testing-library/react"
+import Header from '../components/Header'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import appStore from '../store/appStore'
+import '@testing-library/jest-dom'
+
+test('It should load header component with login button and 0 items in cart', () => {
+    render(
+        // <BrowserRouter>
+        //     <Provider store={appStore}>
+        //     </Provider>
+        // </BrowserRouter>
+
+        <Header />
+    )
 })
